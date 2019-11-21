47a6d7993fce411df928542d1a52a0fe21b3f567
unknown <abhibenne@gmail.com>
2019-11-21 20:26:39 +0530

    practical version

--- optimal_path2.c
+++ optimal_path2.c
@@ -17,17 +17,39 @@ int min_dist(const int *dist, int *spt_set)
     return min_idx;
 }
 
-int max_dist(const int *dist, int *spt_set)
+void print_path_once(int graph[VERTEX][VERTEX],const int *parent, int v,int visited[])
 {
-    int max = INT_MIN, max_idx = 0;
+    /* TODO: for large array  stack is better choice */
+    if (parent[v] == -1) {
+        printf("%d ", v);
+        return ;
+    }
+    // trying to alter it
+    graph[parent[v]][v]=graph[parent[v]][v]+1;
+    graph[v][parent[v]]=graph[v][parent[v]]+1;
+    if(visited[v]==0)
+    {
+        visited[v]=1;
+    }
+    if(graph[v][parent[v]]<0)
+    {
+        graph[v][parent[v]]=0;
+        graph[parent[v]][v]=0;
+    }
+    print_path_once(graph,parent, parent[v],visited);
+    printf("%d ", v);
+}
+
+void print_spt_once(int graph[VERTEX][VERTEX],const int *dist, int src,int dest, const int *parent,int visited[])
+{
+    puts("\nVertex    Distance\n");
     int i;
-    for (i = 0; i < VERTEX; i++)
-        if (spt_set[i] == 0 && max < dist[i]) {
-            max     = dist[i];
-            max_idx = i;
-        }
- 
-    return max_idx;
+    for (i = 0; i < VERTEX; i++) {
+       printf(" %2d -> %2d      %2d         ", src, i, dist[i]);
+       putchar('\n');
+    }
+    printf("\nPath\n");
+    print_path_once(graph,parent, dest,visited);
 }
 
 void dijkstra_min_once(int graph[VERTEX][VERTEX], int src,int dest,int visited[])
@@ -42,33 +64,14 @@ void dijkstra_min_once(int graph[VERTEX][VERTEX], int src,int dest,int visited[]
         spt_set[i] = 0;     
     }
  
-    /* distance of source vertex to itself always 0 */
-    dist[src] = 0;
+     dist[src] = 0;
  
-    /* find shortest path for all vertices */
     for (i = 0; i < VERTEX-1; i++) {
-        /* pick minimum vertex of those vertices not processed
-         *  u is always equal to src in the first call of min_dist()
-         *  */
         int u = min_dist(dist, spt_set);
- 
-        /* mark the picked vertex as processed */
         spt_set[u] = 1;
- 
-        /* if current unprocessed vertex is unreachable
-         *  for those disconnected vertex
-         * */
         if (dist[u] == INT_MAX) continue;
- 
-        /* update dist[] of adjacent vertices of the picked vertex */
         int v;
         for (v = 0; v < VERTEX; v++)
-            /* update dist[v] only if
-             *  1) there is a edge from u to v
-             *  2) it's not in spt_set[]
-             *  3) distance of path from src to v through u
-             *    is smaller than current value of dist[v]
-             * */
             if (graph[u][v] && spt_set[v] == 0 &&
                     dist[u] + graph[u][v] < dist[v])
             {
@@ -76,10 +79,8 @@ void dijkstra_min_once(int graph[VERTEX][VERTEX], int src,int dest,int visited[]
                 parent[v] = u;
             }
     }
-    /* print the constructed distance array */
-    print_spt(graph,dist, src,dest, parent);
+    print_spt_once(graph,dist, src,dest, parent,visited);
 }
-
 
 
 
@@ -106,73 +107,17 @@ void print_path(int graph[VERTEX][VERTEX],const int *parent, int v)
  
 void print_spt(int graph[VERTEX][VERTEX],const int *dist, int src,int dest, const int *parent)
 {
-    puts("  Vertex    Distance");
+    puts("\nVertex    Distance");
     int i;
     for (i = 0; i < VERTEX; i++) {
        printf(" %2d -> %2d      %2d         ", src, i, dist[i]);
        putchar('\n');
     }
     // printf(" %2d -> %2d      %2d         ", src,dest, dist[4]);
-    printf("Path\n");
+    printf("\nPath\n");
     print_path(graph,parent, dest);
 }
  
-void dijkstra_max(int graph[VERTEX][VERTEX], int src,int dest)
-{
-    /* dist[] will hold the shortest distance sourced src */
-    int dist[VERTEX];
-    /* Shortest Path Tree Set  true if a vertex in included in SPT
-     *  or shortest distance from src to i is finalized
-     * */
-    int spt_set[VERTEX];
- 
-    /* parent array to store shortest path tree */
-    int parent[VERTEX];
-    parent[src]  = -1;            /* assuming source have no parent */
-  int i;
-    for (i = 0; i < VERTEX; i++) {
-        dist[i]    = INT_MIN;       /* unreachable */
-        spt_set[i] = 0;         /* not in SPT set */
-    }
- 
-    /* distance of source vertex to itself always 0 */
-    dist[src] = 0;
- 
-    /* find shortest path for all vertices */
-    for (i = 0; i < VERTEX-1; i++) {
-        /* pick minimum vertex of those vertices not processed
-         *  u is always equal to src in the first call of min_dist()
-         *  */
-        int u = max_dist(dist, spt_set);
- 
-        /* mark the picked vertex as processed */
-        spt_set[u] = 1;
- 
-        /* if current unprocessed vertex is unreachable
-         *  for those disconnected vertex
-         * */
-        if (dist[u] == INT_MIN) continue;
- 
-        /* update dist[] of adjacent vertices of the picked vertex */
-        int v;
-        for (v = 0; v < VERTEX; v++)
-            /* update dist[v] only if
-             *  1) there is a edge from u to v
-             *  2) it's not in spt_set[]
-             *  3) distance of path from src to v through u
-             *    is smaller than current value of dist[v]
-             * */
-            if (graph[u][v] && spt_set[v] == 0 &&
-                    dist[u] + graph[u][v] > dist[v])
-            {
-                dist[v]   = dist[u] + graph[u][v];
-                parent[v] = u;
-            }
-    }
- 
-    /* print the constructed distance array */
-    print_spt(graph,dist, src,dest, parent);
-}
 
 void dijkstra_min(int graph[VERTEX][VERTEX], int src,int dest)
 {
@@ -322,11 +267,11 @@ void nearest_node(int bus_graph[VERTEX][VERTEX],int profit_graph[VERTEX][VERTEX]
     }
     if(neighbours==0)
     {
-    	printf("no immediate neighbours, maybe take an alternate route?\n");
+    	printf("\nNo immediate neighbours, maybe take an alternate route?\n");
 	}
 	else
 	{
-		printf("\nimmediate neighbour nodes are present,bus plus walk can be done\n");
+		printf("\nImmediate neighbour nodes are present,bus plus walk can be done\n");
 		printf("%d %d %d\n",queue[0],queue[1],queue[2]);
 	    // dijkstra_mini(bus_graph,profit_graph,queue,source,destination);
         int min=INT_MAX-1;int mini=VERTEX;
@@ -348,38 +293,38 @@ void nearest_node(int bus_graph[VERTEX][VERTEX],int profit_graph[VERTEX][VERTEX]
 	}
 }
 
-void bfs(int graph[VERTEX][VERTEX],int visit[],int order[],int source)
-{
-    int q[VERTEX];
-    int count=0,j;
-    int visited[VERTEX];
-    for(j=0;j<VERTEX;j++)
-    {
-     visited[j]=0;
-    }
-    int top=-1;int index=0;
-    visited[source]=1;top++;
-    q[0]=source;int front=0;
-    while(front!=(top+1))
-   {
-        printf("inside bfs: %d",q[top]);
-        printf("top value is %d\n",top);
-        int a=q[front++];
-        int i;
-        for(i=0;i<VERTEX;i++)
-        {
-            if(!visited[i] && graph[a][i]!=0)
-            {
-                if(visit[i]==1)
-                {
-                    order[index++]=i;
-                }
-                visited[i]=1;
-                q[++top]=i;
-            }
-        }
-   }
-}
+// void bfs(int graph[VERTEX][VERTEX],int visit[],int order[],int source)
+// {
+//     int q[VERTEX];
+//     int count=0,j;
+//     int visited[VERTEX];
+//     for(j=0;j<VERTEX;j++)
+//     {
+//      visited[j]=0;
+//     }
+//     int top=-1;int index=0;
+//     visited[source]=1;top++;
+//     q[0]=source;int front=0;
+//     while(front!=(top+1))
+//    {
+//         printf("inside bfs: %d",q[top]);
+//         printf("top value is %d\n",top);
+//         int a=q[front++];
+//         int i;
+//         for(i=0;i<VERTEX;i++)
+//         {
+//             if(!visited[i] && graph[a][i]!=0)
+//             {
+//                 if(visit[i]==1)
+//                 {
+//                     order[index++]=i;
+//                 }
+//                 visited[i]=1;
+//                 q[++top]=i;
+//             }
+//         }
+//    }
+// }
 
 void DFS(int graph[VERTEX][VERTEX],int visited[],int visit[],int order[],int source,int index)
 {
@@ -455,7 +400,7 @@ int i,j;
         printf("\nBus route graph\n");
         display(graph3,vertex);
     }
-    int source,destination,num_travelers;
+    int source,destination,num_travelers;int visited[vertex];
     printf("\nEnter your choice:\n1) Traveler or travelers which have to visit places\n2) Shortest route from one place to another based on distance and traffic\n");
     printf("3)To report connection changes from place A to B\n4)To display shortest route to place via public trasnport\n5)To display the graphs\n");
     scanf("%d",&choice);
@@ -463,7 +408,7 @@ int i,j;
     switch(choice)
     {
         case 1:
-            int visited[vertex];
+//            int visited[vertex];
             printf("Suggesting path for travelers\n");
             printf("Traveler based on locations\n");
             printf("Enter source and destination vertex\n");
@@ -475,20 +420,21 @@ int i,j;
             for(i=0;i<vertex;i++)
             {
                 ordered_loc[i]=-1;
-                visited[vertex]=0;
+                visited[i]=0;
                 printf("\n%d =>");
                 scanf("%d",&visit_loc[i]);
             }
-            dfs(graph1,visit_loc,ordered_loc,source);
+            dfs(graph1,visit_loc,ordered_loc,source);int ref;
             dijkstra_min_once(graph1,source,ordered_loc[0],visited);
             for(i=0;i<(vertex-1) && ordered_loc[i+1]!=-1;i++)
             {
+                if(visited[ordered_loc[i+1]]==0)
                 dijkstra_min_once(graph1,ordered_loc[i],ordered_loc[i+1],visited);
             }
             dijkstra_min(graph1,ordered_loc[i],destination);
             break;
         case 2:
-            printf("shortest route only based on distance and traffic");
+            printf("Shortest route only based on distance and traffic\n");
             printf("Traveler based on locations\n");
             printf("Enter source and destination vertex\n");
             scanf("%d %d",&source,&destination);
@@ -563,7 +509,7 @@ int i,j;
             }
             break;
         case 5:
-            printf("\nDistance graph");
+            printf("\nDistance graph\n");
             display(graph1,vertex);
             if(choice1==1)
             {
